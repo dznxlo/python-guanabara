92966b04443c6ddb071ef0c6a0ae34ed42a036f9
dnzxlo <pantojadan29@gmail.com>
2025-07-21 12:45:12 -0300

    exercicios 2 a 4

---
+++ ex002.py
@@ -0,0 +1,2 @@
+x = input('insira seu nome:')
+print(f'welcome {x}')
---
+++ ex003.py
@@ -0,0 +1,3 @@
+x = int(input('insira um valor:'))
+y = int(input('insira outro valor:'))
+print(f'a soma entre {x} e {y} é igual a {x+y}')
