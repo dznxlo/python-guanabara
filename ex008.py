97eaa08ecdc83153a1642098a84563a8888d3f1c
dnzxlo <pantojadan29@gmail.com>
2025-07-22 11:38:10 -0300

    exercicios 8 a 10

---
+++ ex008.py
@@ -0,0 +1,3 @@
+x = float(input('insira um valor em metros:'))
+print(f'a distância {x:.0f}m corresponde a:')
+print(f'{x/1000}km, {x/100}hm, {x/10}dam, {x*10:.0f}dm, {x*100:.0f}cm, {x*1000:.0f}mm')
---
+++ ex010.py
@@ -0,0 +1,2 @@
+x = float(input('insira um valor em reais: R$'))
+print(f'com R${x:.2f} é possível comprar US${x/5.57:.2f}')
